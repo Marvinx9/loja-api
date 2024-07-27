25b218b8728c71c7ccf54733262ff43b075f1705
Afranio Nunes Dantas <afraniodantas0224@gmail.com>
2024-07-27 00:38:21 -0300

    feat: align entity of correct decorators

--- src/config/postgres.config.service.ts
+++ src/config/postgres.config.service.ts
@@ -5,6 +5,7 @@ import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
 @Injectable()
 export class PostgresConfigService implements TypeOrmOptionsFactory {
   constructor(private configService: ConfigService) {}
+
   createTypeOrmOptions(): TypeOrmModuleOptions {
     return {
       type: 'postgres',
