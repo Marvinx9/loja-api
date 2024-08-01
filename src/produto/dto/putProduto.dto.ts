b606607fcd9f9ad1b2292149b18a0f8f32f7521f
Afranio Dantas <afranio.dantas@livsaude.com.br>
2024-08-01 13:08:00 -0300

    feat: partialtype usecase add on api

--- src/produto/dto/putProduto.dto.ts
+++ src/produto/dto/putProduto.dto.ts
@@ -1,38 +1,4 @@
-import { IsArray, IsNumber, IsOptional, IsString } from 'class-validator';
-import { Type } from 'class-transformer';
-import { ImagemProdutoDto } from './imagemProduto.dto';
-import { CaracteristicaProdutoDto } from './caracteristicaProduto.dto';
+import { PartialType } from '@nestjs/mapped-types';
+import { PostProdutoDto } from './postProduto.dto';
 
-export class PutProdutoDto {
-  @IsOptional()
-  @IsString()
-  nome?: string;
-
-  @IsOptional()
-  @IsNumber()
-  @Type(() => Number)
-  valor?: number;
-
-  @IsOptional()
-  @IsNumber()
-  @Type(() => Number)
-  quantidade?: number;
-
-  @IsOptional()
-  @IsString()
-  descricao?: string;
-
-  @IsOptional()
-  @IsString()
-  categoria?: string;
-
-  @IsOptional()
-  @IsArray()
-  @Type(() => CaracteristicaProdutoDto)
-  caracteristicas?: CaracteristicaProdutoDto[];
-
-  @IsOptional()
-  @IsArray()
-  @Type(() => ImagemProdutoDto)
-  imagens?: ImagemProdutoDto[];
-}
+export class PutProdutoDto extends PartialType(PostProdutoDto) {}
--- src/usuario/dto/putUsuario.dto.ts
+++ src/usuario/dto/putUsuario.dto.ts
@@ -1,17 +1,4 @@
-import { IsEmail, IsOptional, IsString, MinLength } from 'class-validator';
-import { EmailEhUnico } from '../validacao/email-eh-unico.validator';
+import { PartialType } from '@nestjs/mapped-types';
+import { PostUsuarioDto } from './postUsuario.dto';
 
-export class PutUsuarioDto {
-  @IsString()
-  @IsOptional({ message: 'Nome não pode ser vazio' })
-  nome?: string;
-
-  @IsEmail(undefined, { message: 'O e-mail informado é inválido' })
-  @EmailEhUnico({ message: 'Já existe um usuário com este e-mail' })
-  @IsOptional()
-  email?: string;
-
-  @MinLength(6, { message: 'Senha precisa ter pelo menos 6 caracteres' })
-  @IsOptional()
-  senha?: string;
-}
+export class PutUsuarioDto extends PartialType(PostUsuarioDto) {}
